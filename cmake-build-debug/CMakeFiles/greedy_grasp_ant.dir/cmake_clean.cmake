b5339e5efa7368afb8be8c7f25ff4f23e4cd91e5
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-05 18:37:04 +0200

    Created project.

---
+++ cmake-build-debug/CMakeFiles/greedy_grasp_ant.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greedy_grasp_ant.dir/main.c.o"
+  "greedy_grasp_ant.pdb"
+  "greedy_grasp_ant"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/greedy_grasp_ant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/greedy_grasp_ant.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for greedy_grasp_ant.
+# This may be replaced when dependencies are built.
